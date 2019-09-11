92558731518a406a56d8f5fcb2bc64f20dcfd16f
anbya <anbyaalibia@gmail.com>
2019-09-11 12:12:56 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import './assets/font-awesome/css/font-awesome.min.css';
+import Modal1 from "./commponents/Modal1";
+import Modal2 from "./commponents/Modal2";
 
-class ModalExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    };
-
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }));
-  }
-
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <Button color="danger" onClick={this.toggle}>Open Modal</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Modal title 1</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
+function App() {
+  return (
+    <div className="container-fluid" style={{backgroundColor:"#efefef", height:"100vh", width:"100vw"}}>
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <div className="row justify-content-center">
+            <div className="col-6">
+              <Modal1 />
+            </div>
+            <div className="col-6">
+              <Modal2 />
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <div className="row justify-content-center">
+            <div className="col-6">
+              <Modal1 />
+            </div>
+            <div className="col-6">
+              <Modal2 />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ModalExample;
-
+export default App;
